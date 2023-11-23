57648719c24ca6ac534c5699148229bbaf085b67
wissamn7 <120219581+wissamn7@users.noreply.github.com>
2023-11-23 17:46:00 +0200

    Update CandidatesUtil.java

--- simple-spring-boot-rest-api/src/main/java/com/divit/springboot/application/util/CandidatesUtil.java
+++ simple-spring-boot-rest-api/src/main/java/com/divit/springboot/application/util/CandidatesUtil.java
@@ -18,11 +18,7 @@ public final class CandidatesUtil {
 	 */
 	public static List<Candidate> getCandidates() {
 		var candidateList = List.of(
-				new Candidate("John", 2, Set.of("java", "c#", "c++", "golang")),
-				new Candidate("David", 1, Set.of("javascript", "c++")),
-				new Candidate("Diana", 4, Set.of("java", "python")),
-				new Candidate("June", 2, Set.of("ruby", "aws", "docker")),
-				new Candidate("April", 3, Set.of("python", "Erlang", "docker")));
+				new Candidate("Wissam Nasreddine", 222074, Set.of("python", "c-sharp", "c++")));
 
 		return candidateList;
 	}
